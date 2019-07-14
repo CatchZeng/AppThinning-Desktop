be94f6e45361b0b23ede8fa769643d7c3020a4b6
CatchZeng <catchzenghh@gmail.com>
2019-07-14 13:22:07 +0800

    feat: add types

--- src/appthinning/index.js
+++ src/appthinning/index.js
@@ -3,15 +3,15 @@ import child_process from 'child_process';
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-function appthinning(projectPath) {
+function appthinning(projectPath, types) {
     const cmdPath = srcPath("appthinning/index.js")
-    child_process.exec("node "+cmdPath+" -d " + projectPath, function(error, stdout, stderr) {
+    const cmd = "node "+cmdPath+" -d " + projectPath + " -t "+ "\"" + types +"\""
+    child_process.exec(cmd, function(error, stdout, stderr) {
         if(error){
             console.log(error);
         }else{
             console.log("成功 :" + stdout);
         }
-
         if(stderr) {
           console.log(stderr);
         }
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -3,6 +3,12 @@
     <p class="title"> AppThinning </p>
     <p class="path"> Your project path </p>
     <input type="text" v-model="path"/>
+    <p> Support types </p>
+    <label><input v-model="jpgEnabled" type="checkbox"/>jpg</label> 
+    <label><input v-model="jpegEnabled" type="checkbox"/>jpeg</label> 
+    <label><input v-model="pngEnabled" type="checkbox"/>png</label> 
+    <label><input v-model="gifEnabled" type="checkbox"/>gif</label> 
+    <label><input v-model="svgEnabled" type="checkbox"/>svg</label>
     <div class="button">
       <button class="start" v-on:click="onStartButtonClicked">start</button>
     </div>
@@ -11,23 +17,51 @@
 
 <script>
 import appthinning from '../appthinning'
+import { type, constants } from 'os';
 
 export default {  
   data: function () {
       return {
-        path: ""
+        path: "",
+        pngEnabled: true,
+        jpgEnabled: true,
+        jpegEnabled: true,
+        gifEnabled: true,
+        svgEnabled: true
       }
   },
   methods: {
     onStartButtonClicked: function (event) {
-      console.log("on start button clicked")
+      const types = this.getTypes()
       console.log("path: "+ this.path)
-      appthinning(this.path)
+      console.log("types: "+ types)
+      appthinning(this.path, types)
+    },
+    getTypes: function() {
+      var types = ""
+      if(this.pngEnabled){
+        types += "png|"
+      }
+      if(this.jpgEnabled){
+        types += "jpg|"
+      }
+      if(this.jpegEnabled){
+        types += "jpeg|"
+      }
+      if(this.gifEnabled){
+        types += "gif|"
+      }
+      if(this.svgEnabled){
+        types += "svg|"
+      }
+      if (types[types.length-1] === "|"){
+        types = types.substring(0, types.length - 1)
+      }
+      return types
     }
   },
 }
 </script>
 
 <style scoped>
-
 </style>
