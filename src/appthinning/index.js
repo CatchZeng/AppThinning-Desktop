cb925fcfdeb3e4d44b84496b51f1616fa5eecdde
CatchZeng <catchzenghh@gmail.com>
2019-07-16 22:38:37 +0800

    feat: add node

--- src/appthinning/index.js
+++ src/appthinning/index.js
@@ -5,7 +5,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 
 function appthinning(projectPath, types, miniSize, maxSize, compression, key) {
     const cmdPath = srcPath("appthinning/index.js")
-    var cmd = "node "+cmdPath
+    var cmd = nodePath()+" "+cmdPath
     if (projectPath) {
       cmd += " -d " + projectPath
     }
@@ -42,6 +42,13 @@ function srcPath(subPath) {
     return path.join(__dirname,'../../node_modules/',subPath)
   }
   return path.join(__dirname,'/node_modules/', subPath)
+}
+
+function nodePath() {
+  if (isDevelopment) {
+    return "node"
+  }
+  return path.join(__dirname,'/node/node')
 }
 
 export default appthinning
